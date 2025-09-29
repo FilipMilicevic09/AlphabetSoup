2316ff24ad8496e9c5cfad05fa058fa54b352159
FilipMilicevic09 <filip.milicevic009@gmail.com>
2025-09-29 18:30:03 +0000

    sdfds

--- Soup.java
+++ Soup.java
@@ -35,7 +35,7 @@ public class Soup {
 
     //Use Math.random() to get a random character from the letters string and return it.
     public char randomLetter(){
-    String Rodwell = letters.charAt((int)(Math.random() * letters.length()-1));
+    char Rodwell = letters.charAt((int)(Math.random() * letters.length()-1));
     return Rodwell;
     }
 
@@ -43,7 +43,8 @@ public class Soup {
     //returns the letters currently stored with the company name placed directly in the center of all
     //the letters
     public String companyCentered(){
-        return "";
+       String a = letters.substring(0,(letters.length()-1)/2);
+       String b = 
 
     }
 
